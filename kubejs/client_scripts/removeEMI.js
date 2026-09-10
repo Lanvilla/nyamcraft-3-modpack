587aea3bbcc5764900570a119524e85fc2819d07
Lanvilla <33900813+Lanvilla@users.noreply.github.com>
2026-09-10 04:16:03 +0300

    recipe fixes
    `croptopia:butter` - теперь рецепты, которые требовали масло из `farm_and_charm` принимают `croptopia` как ингредиент
    `farm_and_charm:barley` - один рецепт требовал не ту ячмень, теперь возможно скрафтить `farmandcharm:barley_patties_with_potatoes` для достижения
    `farm_and_charm:flour` - теперь все рецепты принимают любой тип муки, но из пшеницы теперь крафтится только `croptopia:flour`, старую муку можно конвертировать в новую путем крафта ее в ящик муки и обратно 9->1->9 (если у кого-то она есть)
    `farmers/fungi..` `delight:*_crate` - теперь ящики можно создавать

--- kubejs/client_scripts/removeEMI.js
+++ kubejs/client_scripts/removeEMI.js
@@ -4,7 +4,6 @@ RecipeViewerEvents.removeEntries("item", event => {
 	event.remove('farmersdelight:dog_food')
 	event.remove('croptopia:dough')
 	event.remove('croptopia:coffee')
-	event.remove('croptopia:butter')
 
 	global.removedItems.forEach((item) => {
 		event.remove(item)
