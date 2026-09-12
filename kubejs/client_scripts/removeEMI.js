277ea059da190340bd1affbb9c7758e88aedb103
Lanvilla <33900813+Lanvilla@users.noreply.github.com>
2026-09-13 01:42:01 +0300

    dough + noodle fix

--- kubejs/client_scripts/removeEMI.js
+++ kubejs/client_scripts/removeEMI.js
@@ -2,6 +2,7 @@ RecipeViewerEvents.removeEntries("item", event => {
 	event.remove('/^tinymultiblocklib:/')
 
 	event.remove('farmersdelight:dog_food')
+	event.remove('farmersdelight:raw_pasta')
 	event.remove('croptopia:dough')
 	event.remove('croptopia:coffee')
 
